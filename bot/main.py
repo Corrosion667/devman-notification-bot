1110215e0e7792402272628cf7fd4b552c049618
Артём Нечай <a.nechai@okenit.ru>
2022-03-22 11:12:37 +0300

    get rid of global variables

--- bot/main.py
+++ bot/main.py
@@ -8,13 +8,6 @@ import telegram
 from dotenv import load_dotenv
 from requests.exceptions import ConnectionError, HTTPError, ReadTimeout
 
-load_dotenv()
-
-DEVMAN_TOKEN = os.getenv('DEVMAN_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-USERNAME = os.getenv('USERNAME', 'friend')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-
 LONG_POLLING_URL = 'https://dvmn.org/api/long_polling/'
 LONG_POLLING_TIMEOUT = 90
 CONNECTION_LOST_TIMEOUT = 60
@@ -29,19 +22,28 @@ NEGATIVE_RESULT = 'Unfortunately, some mistakes have been found in your task. Pl
 class DevmanBot(object):
     """Class for devman notification bot."""
 
-    def __init__(self):
-        """Initiate bot instance."""
+    def __init__(self, devman_token, telegram_token, telegram_chat_id, username):
+        """Initiate bot instance.
+
+        Args:
+            devman_token: personal student token from *dvmn.org* to use its API.
+            telegram_token: bot token from @BotFather in telegram.
+            telegram_chat_id: id of a person from @userinfobot.
+            username: the name with which the bot will address the userю
+        """
         self.headers = {
-            'Authorization': f'Token {DEVMAN_TOKEN}',
+            'Authorization': f'Token {devman_token}',
         }
-        self.bot = telegram.Bot(token=TELEGRAM_TOKEN)
+        self.bot = telegram.Bot(token=telegram_token)
+        self.telegram_chat_id = telegram_chat_id
+        self.username = username
         self.url = LONG_POLLING_URL
 
     def start(self):
         """Start the bot."""
         self.bot.send_message(
-            chat_id=TELEGRAM_CHAT_ID,
-            text=f'Hello, {USERNAME}!',
+            chat_id=self.telegram_chat_id,
+            text=f'Hello, {self.username}!',
         )
         request_time = time.time()
         while True:
@@ -62,7 +64,7 @@ class DevmanBot(object):
                 response.raise_for_status()
             except HTTPError as exc:
                 self.bot.send_message(
-                    chat_id=TELEGRAM_CHAT_ID,
+                    chat_id=self.telegram_chat_id,
                     text=HTTP_ERROR_NOTIFICATION.format(exception=exc, timeout=HTTP_ERROR_TIMEOUT),
                 )
                 time.sleep(HTTP_ERROR_TIMEOUT)
@@ -85,17 +87,32 @@ class DevmanBot(object):
         lesson_title = work_information.get('lesson_title')
         lesson_url = work_information.get('lesson_url')
         notification = REVIEW_NOTIFICATION.format(
-            user=USERNAME,
+            user=self.username,
             title=lesson_title,
             link=lesson_url,
             result=NEGATIVE_RESULT if is_work_failed else POSITIVE_RESULT,
         )
         self.bot.send_message(
-            chat_id=TELEGRAM_CHAT_ID,
+            chat_id=self.telegram_chat_id,
             text=notification,
         )
 
 
-if __name__ == '__main__':
-    bot = DevmanBot()
+def main():
+    """Execute bot as a script."""
+    load_dotenv()
+    devman_token = os.getenv('DEVMAN_TOKEN')
+    telegram_token = os.getenv('TELEGRAM_TOKEN')
+    telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
+    username = os.getenv('USERNAME', 'friend')
+    bot = DevmanBot(
+        devman_token,
+        telegram_token,
+        telegram_chat_id,
+        username,
+    )
     bot.start()
+
+
+if __name__ == '__main__':
+    main()
